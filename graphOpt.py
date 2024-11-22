cfcfd11c8fb4ab8d30d7648f12ea25cc281cb13c
Vladislav Suchkov <v.suchkov@yadro.com>
2024-11-23 00:53:17 +0300

    Dijkstra method

---
+++ graphOpt.py
@@ -0,0 +1,62 @@
+from heapq import heappop, heappush
+from dataclasses import dataclass, field
+
+@dataclass(order=True)
+class Node:
+    idx: int=field(compare=False)
+    value: float
+
+class Graph:
+    def __init__(self):
+        self.weights = []
+        self.adjLists = []
+        self.NumVertices = 0
+        self.marks = []
+        self.constant = []
+    def __init__(self, weights):
+        self.NumVertices = len(weights[0])
+        self.weights = weights
+        self.adjLists = []
+        self.marks = []
+        self.constant = [0 for _ in range(self.NumVertices)]
+        for i in range(self.NumVertices):
+            adjacent = []
+            for j in range(self.NumVertices):
+                if (weights[i][j] >= 0):
+                    adjacent.append(j)
+            self.adjLists.append(adjacent)
+
+    def cost(self, i, j):
+        return self.weights[i][j]
+
+def Dijkstra(g, start = 0, final = 0):
+    marks = [float('inf') for i in range(g.NumVertices)]
+    marks[start] = 0
+
+    prev = [0 for i in range(g.NumVertices)]
+    temp = []
+    v = start
+    while(v != final):
+        print(f"V={v}")
+        for adj in g.adjLists[v]:
+            heappush(temp, Node(adj, marks[v] + g.cost(v, adj)))
+
+        cst = heappop(temp)
+        newv = cst.idx
+        prev[newv] = v
+        marks[newv] = cst.value
+        g.constant[newv] = 1
+        v = newv
+
+    path = [v]
+    while (v != start):
+        path.append(prev[v])
+        v = prev[v]
+    path = list(reversed(path))
+    g.marks = marks
+
+    return marks[final], path
+
+     
+
+        
---
+++ main.py
@@ -0,0 +1,40 @@
+import graphOpt as gopt
+import networkx as nx
+import matplotlib.pyplot as plt
+
+
+G = gopt.Graph([[-1, -1, 1, -1, -1, 1, -1], 
+                [-1, -1, 2, 4, -1, 1, 6],
+                [-1, -1, -1, 2, -1, -1, 5],
+                [-1, -1, -1, -1, 2, 3, -1],
+                [-1, -1, -1, -1, -1, 2, 2],
+                [-1, -1, -1, -1, -1, -1, 2],
+                [-1, -1, -1, -1, -1, -1, -1]])
+cost, path = gopt.Dijkstra(G, 0, 6)
+print(cost)
+print(path)
+
+vgraph = nx.DiGraph()
+vgraph.add_nodes_from([i for i in range(G.NumVertices)])
+
+i = 0
+for adj in G.adjLists:
+    for j in adj:
+        vgraph.add_edge(i, j, weight=G.cost(i, j))
+    i += 1
+
+
+
+pos = nx.nx_pydot.graphviz_layout(vgraph, root=0, prog='dot')
+
+state_pos_cst = {n: (x+12, y+12) for n, (x,y) in pos.items() if G.constant[n] == True}
+state_pos_tmp = {n: (x+12, y+12) for n, (x,y) in pos.items() if G.constant[n] == False}
+
+nx.draw_networkx(vgraph, pos, with_labels=True, font_weight='bold')
+edge_labels = nx.get_edge_attributes(vgraph, 'weight')
+nx.draw_networkx_edge_labels(vgraph, pos, edge_labels=edge_labels)
+node_labels_cst = {n: G.marks[n] for n in vgraph.nodes if G.constant[n] == True}
+node_labels_tmp = {n: G.marks[n] for n in vgraph.nodes if G.constant[n] == False}
+nx.draw_networkx_labels(vgraph, state_pos_cst, labels = node_labels_cst, font_color='blue')
+nx.draw_networkx_labels(vgraph, state_pos_tmp, labels = node_labels_tmp, font_color='red')
+plt.show()
